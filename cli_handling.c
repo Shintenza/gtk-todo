522e9bddc657803ecc4af941c86e2dc4ab8b9737
Shintenza <kam.kuziora@gmail.com>
2021-12-05 00:13:28 +0100

    planned delete funcions working

--- cli_handling.c
+++ cli_handling.c
@@ -1,5 +1,6 @@
 #include "include/structs.h"
 #include "include/messages.h"
+#include <stdio.h>
 #define MAX_ENTRIES 1000
 #define MAX_NAME_LENGTH 125
 #define MAX_DESC_LENGTH 2000
@@ -100,29 +101,58 @@ void listing_handling (int *entries_counter, struct CliTask *tasks_arr, int argc
         }
     }
 }
-void delete_handling (int argc, char **argv, int tasks_array_len, int first_finished_task, struct CliTask *tasks_arr) {
+void delete_handling (int argc, char **argv, int tasks_array_len, int first_finished_task, struct CliTask *tasks_arr, sqlite3 *db) {
     int db_user_given_id = -1;
     int user_given_id = -1;
+    int rc;
     char *error;
+    char *sql = malloc(500);
+    char user_input[4];
+    char *return_message = malloc(1000);
+    char *db_error_msg = 0;
     int i;
-    struct CliTask chosen_task;
+    struct CliTask chosen_task = {0};
     if (argc < 3) printf("Musisz podać numer zadania lub jego id z bazy danych po fladze -b\n");
     if (check_if_flag_exists(argc, argv, "-b") || check_if_flag_exists(argc, argv, "-n")){
-
         db_user_given_id = check_if_flag_exists(argc, argv, "-b") ? strtol(argv[3], &error, 10) : -1;
         user_given_id = check_if_flag_exists(argc, argv, "-n") ? strtol(argv[3], &error, 10) : -1;
         if (*error!='\0') {
             printf("Podana wartość musi być liczbą!\n");
+            free(sql);
+            free(return_message);
+            return;
+        }
+    } else if (argc == 3 && check_if_flag_exists(argc, argv, "-Ao")) {
+        for (i = 0; i<tasks_array_len; i++) {
+            if (tasks_arr[i].finished == 1) {
+                sprintf(sql, "DELETE FROM tasks WHERE rowid=%d", tasks_arr[i].rowid);
+                rc = sqlite3_exec(db, sql, NULL, NULL, &db_error_msg);
+                if (rc!=SQLITE_OK) {
+                    fprintf(stderr, "SQL err, %s\n", sqlite3_errmsg(db));
+                    sqlite3_free(db_error_msg);
+                    sqlite3_close(db);
+                    free(sql);
+                    free(return_message);
+                    return;
+                }
+            }
+            sprintf(return_message, "Pomyślnie usunięto zadanie: %s z bazy danych!\n", tasks_arr[i].name);
+            free(sql);
+            free(return_message);
             return;
         }
     } else if (argc == 3) {
         user_given_id = strtol(argv[2], &error, 10);
         if (*error!='\0') {
             printf("Podana wartość musi być liczbą!\n");
+            free(sql);
+            free(return_message);
             return;
         }
     } else {
-        printf("Niepoprawna flaga! Użyj --help aby otrzymać pomoc.\n");
+        printf("Niepoprawne użycie komendy! Użyj --help aby otrzymać pomoc.\n");
+        free(sql);
+        free(return_message);
         return;
     }
     if (db_user_given_id>=0) {
@@ -138,29 +168,68 @@ void delete_handling (int argc, char **argv, int tasks_array_len, int first_fini
             }
         }
     }
-    printf("Działanie na tasku o nazwie: %s\n", chosen_task.name);
+    if (chosen_task.unix_time == 0) {
+        printf("Nie znaleziono takiego zadania w naszej bazie danych!\n");
+        free(sql);
+        free(return_message);
+        return;
+    }
+    if (chosen_task.finished==0) {
+        sprintf(return_message, "Czy chcesz przenieść zadanie %s, do zadań ukończoncyh?\nWpisz tak/nie: ", chosen_task.name);
+        sprintf(sql, "UPDATE tasks SET finished = 1 WHERE rowid=%d", chosen_task.rowid);
+    }
+    if (chosen_task.finished==1) {
+        sprintf(return_message, "Wybrane zadanie: %s, ma status ukończonego. Czy chcesz usunąć dany wpis z bazy danych?\nWpisz tak/nie: ", chosen_task.name);
+        sprintf(sql, "DELETE FROM tasks WHERE rowid=%d", chosen_task.rowid);
+    }
+    printf("%s", return_message);
+    fgets(user_input, 4, stdin);
+
+    if (strcmp(user_input, "tak")!=0) {
+        free(sql);
+        free(return_message);
+        return;
+    }
+
+    rc = sqlite3_exec(db, sql, NULL, NULL, &db_error_msg);
+    if (rc!=SQLITE_OK) {
+        fprintf(stderr, "SQL err, %s\n", sqlite3_errmsg(db));
+        sqlite3_free(db_error_msg);
+        sqlite3_close(db);
+        free(sql);
+        free(return_message);
+        return;
+    }
+    free(sql);
+    free(return_message);
+    printf("Operacja przebiegła pomyślnie!!!\n");
 }
-void init_load(sqlite3 *db, char *err_msg, struct CliTask *tasks_arr, int *entries_counter) {
+void init_load(sqlite3 *db, struct CliTask *tasks_arr, int *entries_counter) {
     char *sql = "SELECT rowid, task_name, task_desc, date_string, date, importance, finished FROM tasks ORDER BY finished, date ASC;"; 
+    char *db_error_msg;
     struct CliLoadTaskParams params;
     int rc;
 
     params.tasks_arr = tasks_arr;
     params.entries_counter = entries_counter;
-    rc = sqlite3_exec(db, sql, cli_load_tasks_callback, &params, &err_msg);
-    /* TODO make error handling */
+    rc = sqlite3_exec(db, sql, cli_load_tasks_callback, &params, &db_error_msg);
+    if (rc!=SQLITE_OK) {
+        fprintf(stderr, "SQL err, %s\n", sqlite3_errmsg(db));
+        sqlite3_free(db_error_msg);
+        sqlite3_close(db);
+        return;
+    }
 }
 
 int cli_handling (int argc, char **argv, struct DbElements *db_elements) {
     sqlite3 *db = db_elements->db;
-    char *err_msg = db_elements->err_msg;
     char *main_flag = argv[1];
-    struct CliTask tasks_arr[MAX_ENTRIES] = {{0}};
     int entries_counter = 0;
     int tasks_array_len = 0;
     int first_finished_task = 0;
+    struct CliTask tasks_arr[MAX_ENTRIES] = {{0}};
 
-    init_load(db, err_msg, tasks_arr, &entries_counter);
+    init_load(db, tasks_arr, &entries_counter);
     tasks_arr_length(tasks_arr, &tasks_array_len, &first_finished_task);
 
     if (strcmp(main_flag, "--help") ==0 || strcmp(main_flag, "-h")==0) {
@@ -168,7 +237,7 @@ int cli_handling (int argc, char **argv, struct DbElements *db_elements) {
     } else if(strcmp(main_flag, "-L")==0 || strcmp(main_flag, "-l")==0) {
         listing_handling(&entries_counter, tasks_arr, argc, argv, &tasks_array_len, &first_finished_task);
     } else if(strcmp(main_flag, "-D")==0 || strcmp(main_flag, "-d")==0) {
-        delete_handling(argc, argv, tasks_array_len, first_finished_task, tasks_arr);
+        delete_handling(argc, argv, tasks_array_len, first_finished_task, tasks_arr, db);
     } else {
         printf("Niepoprawna składnia polecenia! Wpisz --help, aby zapoznać się z dozwolonymi poleceniami!\n");
     }
